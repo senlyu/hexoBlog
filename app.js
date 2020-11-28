76e60ffa0d82f7de16997eee79149c77e9966b14
senlyu <sen.lyu@uconn.edu>
2020-11-28 18:21:35 -0500

    adding router adding 404 page

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static(process.env.STATIC_FOLDER))
+// static blog files
+app.use('/blog', express.static(process.env.STATIC_FOLDER))
 
-app.use('*', (req, res, next)=>{
-  console.log(`Got a request: ${req.method} ${req.url}`)
-  next()
-})
+// api router
+const apiRouter = require('./src/apiRouter')
+app.use('/api', apiRouter)
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use('*', (req, res) => {
+  // redirect / to /blog/home
+  if (req.originalUrl==='/') {
+    res.redirect('/blog/home')
+  // redirect 404 to error, because /blog/404 page is exist, should handled in static page
+  // if still here means the page is wrong
+  } else if (req.originalUrl==='/blog/404') {
+    res.redirect('/error')
+  // use /error url to send 503
+  } else if (req.originalUrl==='/error') {
+    res.sendStatus(503)
+  // if not found will go 404 page
+  } else {
+    res.redirect('/blog/404')
+  }
 })
 
 module.exports = app
