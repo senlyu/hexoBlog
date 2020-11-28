e789c12bf7e5e7f261dad052ce111ef456660b93
senlyu <sen.lyu@uconn.edu>
2020-11-27 22:48:58 -0500

    update cred path

--- httpsCred.js
+++ httpsCred.js
@@ -1,8 +1,11 @@
 const fs = require('fs')
 // Certificate
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/senlyu.com/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/senlyu.com/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/senlyu.com/chain.pem', 'utf8');
+const privateKeyPath = process.env.PRIVATE_KEY_PATH
+const certPath = process.env.CERT_PATH
+const caPath = process.env.CA_PATH
+const privateKey = fs.readFileSync(privateKeyPath, 'utf8');
+const certificate = fs.readFileSync(certPath, 'utf8');
+const ca = fs.readFileSync(caPath, 'utf8');
 
 module.exports = {
   key: privateKey,
