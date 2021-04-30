40f6967cef828e22991b95e4fb01cb2188695989
senlyu <me@senlyu.com>
2021-04-30 22:32:11 +0000

    stop use 80

--- production.js
+++ production.js
@@ -3,16 +3,16 @@ const http = require('http');
 const https = require('https');
 const app = require('./app')
 
-const httpServer = http.createServer(app);
-const httpPort = +process.env.HTTP_PORT || 80
-httpServer.listen(httpPort, () => {
-	console.log('HTTP Server running on port ' + httpPort);
-});
-
 if (process.env.NODE_ENV==='production') {
   const credentials = require('./httpsCred')
   const httpsServer = https.createServer(credentials, app);
   httpsServer.listen(443, () => {
     console.log('HTTPS Server running on port 443');
+  });
+} else {
+  const httpServer = http.createServer(app);
+  const httpPort = +process.env.HTTP_PORT || 80
+  httpServer.listen(httpPort, () => {
+    console.log('HTTP Server running on port ' + httpPort);
   });
 }
